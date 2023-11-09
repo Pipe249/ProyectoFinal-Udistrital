81587df79108058e335911f2f55854a7ea84a37b
Andres  Angel <andresangel@sisamlat.com>
2023-11-09 12:46:01 -0500

    manipulación del DOM con js funcionalidad del menú para insertarse en todas las páginas y funcionalidad botón registro

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
-const nav = document.querySelector("#Nav");
-const ButtonOpen = document.querySelector("#ButtonOpen");
-const ButtonClose = document.querySelector("#ButtonClose");
 
-ButtonOpen.addEventListener("click", ()=>{
-    nav.classList.add("visible");
-})
+document.addEventListener('DOMContentLoaded', function () {
+    const navigationMenu = document.getElementById('navigation-menu');
+
+    fetch('../nav.html')
+    .then(response => response.text())
+    .then(data => {
+      navigationMenu.innerHTML = data;
+
+      const ButtonRegister = document.getElementById("ButtonRegister");
+
+      if (ButtonRegister) {
+          ButtonRegister.addEventListener("click", function() {
+              // Tu lógica aquí al hacer clic en el botón de registro
+              console.log("¡Botón de registro clicado!");
+              window.location.href = "Registro.html";
+              // Puedes redirigir a la página de registro u otras acciones
+          });
+      }
+    })
+    .catch(error => console.log(error));
+});
